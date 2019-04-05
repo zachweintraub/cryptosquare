e6f078e77245dfa8f373ad102775437685c1397f
Zach Weintraub <zachweintraub@gmail.com>
2019-04-05 00:12:34 -0700

    create zach branch with rewritten logic from scratch as exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,126 +1,66 @@
-
-let answerStr = '';
-const regEx = /[.,\/#!$%\^&\*;:{}='"\-_`~()\]\[\?]/g;
-
-function clearArrays(){
-  console.log('Clearing out strings!');
-  inputStr = [];
-  answerStr = '';
-};
-
-function generateSquare(myLength, myStr){
-  console.log('generateSquare is being called for '+myStr+" of length "+myLength);
-  var col;
-  var row;
-  var sq = Math.sqrt(myLength);
-  if((sq%1) === 0){
-    console.log('We found a prefect Square!');
-    col = row = sq;
-    console.log("Our array is " + row + " x " + col + "!");
-  }else{
-    col = Math.round(sq);
-    console.log('Number of Columns is ' + col);
-    row = whichSq(col, myLength);
-  }
-  var mySquare = initArray(col, row, myStr);
-  console.log(mySquare);
-  printArray(col, row);
-  answerStr = cryptoLogic(col, row, mySquare);
-  console.log(answerStr);
-  return answerStr;
-};
-
-function whichSq(myCol, myLength){
-  console.log('whichSq is being called for '+myCol);
-  var numChk = Math.pow(myCol, 2);
-  var row;
-  if(numChk < myLength){
-    row = myCol + 1;
-  }else{
-    row = myCol;
-  }
-  return row;
-};
-
-function printArray(myCol, myRow){
-  console.log('Printing out coordinates of our 2D array!');
-  var arrStr = '';
-  for(var j = 0; j < myRow; j++){
-    arrStr += 'R ';
-    for(var i = 0; i < myCol; i++){
-      if(i === 0){
-        arrStr += 'C ';
-      }
-      if(i === (myCol - 1)){
-        arrStr += '[' +j+ ',' +i+ ']\n';
-      }else{
-        arrStr += '[' +j+ ',' +i+ ']';
-      }
-    }
-  }
-  console.log(arrStr);
-};
-
-function initArray(myCol, myRow,  myStr){
-  console.log('Initializing our 2D array that is '+myCol+' x '+myRow);
-  var retArr = [];
-  for(var j = 0; j < myRow; j++){
-    var innerArr = [];
-    for(var i = 0; i < myCol; i++){
-      if(i === myStr.length){
-        console.log('Found end of String, end logic');
-        break;
-      }
-      innerArr.push(myStr[i]);
-      console.log('Adding ' + myStr[i] + ' at (' +i+ ',' +j+ ')');
-    }
-    myStr = myStr.substring(innerArr.length);
-    retArr.push(innerArr);
-  }
-  return retArr;
-};
-
-function cryptoLogic(myCol, myRow, myArr){
-  console.log('myRow = ' +myRow+ ', myCol = '+myCol);
-  var output = '';
-  var secondBr = false;
+function encrypt(str) {
+  var pureStr = str.replace(/[.,\/#!@$%\^&\*;:{}='"\-_`~()\s\]\[\?]/g, '');
+  var cols = Math.ceil(Math.sqrt(pureStr.length));
+  var cryptoSquare = [];
+  var result = '';
   var iter = 0;
-  for(var col = 0; col < myCol; col++) {
-    if(secondBr){
-      break;
-    }
-    for(var row = 0; row < myRow; row++) {
-      console.log('arr['+row+','+col+'] is: ' + myArr[row][col]);
-      if(myArr[row][col] != null){
+  for(var i = 0; i < cols; i++) {
+    cryptoSquare.push(pureStr.substring(0, cols).split(''));
+    pureStr = pureStr.substring(cols);
+  };
+  for(var i = 0; i < cols; i++) {
+    for(var j = 0; j < cryptoSquare.length; j++) {
+      if(cryptoSquare[j][i] != null){
         if(iter === 5){
-          output += ' ';
+          result += ' ';
           iter = 0;
         }
-        output += myArr[row][col];
+        result += cryptoSquare[j][i];
         iter++;
-      }else{
-        console.log('Reached an unpopulated index of myArr at (' + col + ',' + row + ')!' );
-        secondBr = true;
-        break;
       }
     }
   }
-  return output;
-};
+  console.log(cryptoSquare);
+  return result;
+}
+
+function unencrypt(str) {
+  var pureStr = str.replace(/[.,\/#!@$%\^&\*;:{}='"\-_`~()\s\]\[\?]/g, '');
+  var cols = Math.ceil(Math.sqrt(pureStr.length));
+  var cryptoSquare = [];
+  var result = '';
+  for(var i = 0; i < cols; i++) {
+    cryptoSquare.push([]);
+  }
+  console.log(cryptoSquare);
+  for(var i = 0; i < cols; i++) {
+    for(var j = 0; j < cols; j++) {
+      cryptoSquare[i].push(pureStr.charAt(0));
+      pureStr = pureStr.substring(1);
+      }
+    }
+
+  console.log(cryptoSquare);
+  for(var i = 0; i < cols; i++) {
+    for(var j = 0; j < cryptoSquare.length; j++) {
+      if(cryptoSquare[j][i] != null){
+        result += cryptoSquare[j][i];
+      }
+    }
+  }
+  return result;
+}
+
 
 $(document).ready(function(){
-  $('#myForm').submit(function(event){
-    console.log('Submit function is being called correctly!');
+  $('#encryptForm').submit(function(event){
     event.preventDefault();
-    clearArrays();
-    var inputStr = [];
-    var len;
-    inputStr = $('#myInput').val();
-    inputStr = inputStr.replace(regEx, '');
-    inputStr = inputStr.replace(/\s/g,"");
-    console.log('Sentence that was input is: [' + inputStr + ']');
-    len = inputStr.length;
-    generateSquare(len, inputStr);
-    $('#myAns p').text(answerStr);
+    var inputStr = $('#encryptInput').val();
+    $('#encryptAns p').text(encrypt(inputStr));
+  });
+  $('#unencryptForm').submit(function(event){
+    event.preventDefault();
+    var inputStr = $('#unencryptInput').val();
+    $('#unencryptAns p').text(unencrypt(inputStr));
   });
 });
